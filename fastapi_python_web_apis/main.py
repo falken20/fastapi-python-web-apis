e943f268f5ba1ff8b3e7ce6fdb20b1c7aa250683
Richi <r.rodriguez@bbva.com>
2022-11-10 08:54:23 +0100

    Improving API

--- fastapi_python_web_apis/main.py
+++ fastapi_python_web_apis/main.py
@@ -2,6 +2,16 @@
 # fastapi_python_web_apis/main.py
 
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
+
 
 app = FastAPI()
 
@@ -9,3 +19,14 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World!!"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
+
+
+# Read the item from the body of the request as JSON
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
