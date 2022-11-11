d08afe82779fdaa5f1d62c9dc149c2d1a10fac06
Richi <r.rodriguez@bbva.com>
2022-11-11 08:41:12 +0100

    Set new folder tree

--- fastapi_python_web_apis/fastapi_python_web_apis.sh
+++ fastapi_python_web_apis/fastapi_python_web_apis.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
 echo "Create virtual environment..."
-#python -m venv venv
+#../python -m venv venv
 
 echo "Access to virtual environment..."
-#source ./venv/bin/activate
+#source ../venv/bin/activate
 
 echo "Running app...uvicorn url_shortener.main:app --reload"
 # The --reload flag makes sure that your server will reload automatically when you save your application’s code
-uvicorn fastapi_python_web_apis.main:app --reload
+#uvicorn fastapi_python_web_apis.main:app --reload
+uvicorn main:app --reload
 
 echo "Deactivate virtual environment..."
 #deactivate
